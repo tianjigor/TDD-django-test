c4fa9acce5e343d0670a161eeb565b006deb1f36
Tianjigor <541878782@qq.com>
2017-05-02 23:21:50 +0800

    5.6

--- lists/tests.py
+++ lists/tests.py
@@ -28,12 +28,22 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -50,3 +60,5 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
+from lists.models import Item
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request, 'home.html')
-
-    # return render_to_response('home.html')
-
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+    #
     # return render(request, 'home.html', {
-    #     'new_item_text': request.POST['item_text'],
+    #     'new_item_text': request.POST.get('item_text', ''),
     # })
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text=''
 
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text,
     })
