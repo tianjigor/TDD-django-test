a4f84f46342b156a43e7bbc65a20720c41051700
Tianjigor <541878782@qq.com>
2017-05-03 22:18:49 +0800

    6.7

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
@@ -14,6 +14,7 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
