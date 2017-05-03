aa4642c1ab0dae7de18522e11a7e530f25777f67
Tianjigor <541878782@qq.com>
2017-05-03 16:13:21 +0800

    6.3

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,10 +37,11 @@ class NewVisitorTest(LiveServerTestCase):
         # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
-        # 她按回车键后，页面更新了
+        # 她按回车键后，被带到了一个新URL
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
@@ -55,7 +56,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
-        # 页面再次更新，她的清单中显示了这两个待办事项
+
+        # 现在一个叫作弗朗西斯的新用户访问了网站
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来 #➊
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        # 两人都很满意，去睡觉了
 
 
     def check_for_row_in_list_table(self, row_text):
@@ -65,7 +91,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_alist_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
         self.browser.get(self.live_server_url)
